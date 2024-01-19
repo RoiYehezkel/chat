d83da45d3cbf3b561a32dee610c31550d3110972
RoiYehezkel <roi5634@gmail.com>
2024-01-19 14:53:23 +0200

    finish design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+
+          <button>Sign in</button>
+        </form>
+        <p>You do have an account? Register</p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
